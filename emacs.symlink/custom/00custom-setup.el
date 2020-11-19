4fbbaa26d700ea824a6bf089a73863c765c3b496
Luis Urrea <lfurrea@gmail.com>
2020-11-19 10:44:56 -0500

    Updating emacs init files

--- emacs.symlink/custom/00custom-setup.el
+++ emacs.symlink/custom/00custom-setup.el
@@ -12,15 +12,25 @@
 ;; Display continuous lines
 (setq-default truncate-lines nil)
 ;; Do not use tabs for indentation
-(setq-default indent-tabs-mode nil)
+(setq-default indent-tabs-mode t)
 (setq c-basic-indent 2)
+(setq js-indent-level 2
+      indent-tabs-mode t)
 (setq tab-width 2)
 
-(menu-bar-mode t)
-;;(tool-bar-mode -1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(setq inhibit-startup-screen t)
+
+;; set keys for Apple Keyboard
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'super)
+(setq mac-control-modifier 'control)
+(setq ns-function-modifier 'hyper)
+
 
 (require 'solarized-theme)
-;;(load-theme 'solarized-dark)
+(load-theme 'solarized-dark t)
 
 ;; trucate even even when screen is split into multiple windows
 (setq-default truncate-partial-width-windows nil)
@@ -52,19 +62,57 @@
 (setq auto-save-default nil)
 (global-linum-mode 1)
 (global-set-key "\C-x\C-m" 'compile)
-(ido-mode t)
+
 ;;(set-frame-parameter nil 'fullscreen 'fullboth)
+
+;; ivy completion deprecates ido
+(require 'ivy)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(setq ivy-count-format "(%d/%d) ")
+;;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+
+(require 'org)
+;; set key for agenda
+(global-set-key (kbd "C-c a") 'org-agenda)
+;;file to save TODO items
+(setq org-agenda-files (quote ("/Users/lfurrea/dev/todo.org")))
+;; set priority range from A to C with default A
+(setq org-highest-priority ?A)
+(setq org-lowest-priority ?C)
+(setq org-default-priority ?B)
+;; set colors for priority
+(setq org-priority-faces '((?A . (:foreground "#F0DFAF"))
+                           (?B . (:foreground "#LightSteelBlue"))
+                           (?C . (:foreground "#OliveDrab"))
+                           ))
+;;open agenda in current window
+(setq org-agenda-window-setup (quote current-window))
+;;capture todo items using C-c c t
+(define-key global-map (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "/Users/lfurrea/dev/todo.org" "Tasks")
+         "* TODO [#A] %?")))
 
 ;;Projectile
 (require 'grizzl)
 (projectile-global-mode)
 (setq projectile-enable-caching t)
-(setq projectile-completion-system 'grizzl)
+(setq projectile-completion-system 'ivy)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 ;; Press Command-p for fuzzy find in project
-(global-set-key (kbd "s-p") 'projectile-find-file)
+;;(global-set-key (kbd "s-p") 'projectile-find-file)
 ;; Press Command-b for fuzzy switch buffer
-(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+;;(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+;;(define-key projectile-mode-map [?\s-g] 'projectile-grep)
 
+;; Smartparens
+(require 'smartparens-config)
+(add-hook 'elixir-mode-hook #'smartparens-mode)
+;; Run elixir-format on save
+(add-hook 'elixir-mode-hook
+          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 
 ;;Indentation-mode
 (require 'highlight-indentation)
@@ -82,7 +130,10 @@
 (add-hook 'shell-script-mode
 	  (lambda () (highlight-indentation-current-column-mode)))
 
-;;Erlang mode
+;; Elixir mode
+(require 'elixir-mode)
+
+;; Erlang mode
 ;;(add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.8.1/emacs")
 ;;(require 'erlang-start)
 
@@ -100,6 +151,7 @@
 ;;Markdown mode
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
+(setq markdown-command "/usr/local/bin/multimarkdown")
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
